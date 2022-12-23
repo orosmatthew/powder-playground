8355b236898fb2741e6e1a819279edd034133eb2
orosmatthew <orosmatthew@pm.me>
2022-12-23 13:56:01 -0500

    Refactor

--- src/powder_playground.cpp
+++ src/powder_playground.cpp
@@ -64,6 +64,58 @@ struct SimState {
     }
 };
 
+void update_salt(SimState& sim_state, Vector2i particle_pos)
+{
+    Vector2i bottom_pos { particle_pos.x, particle_pos.y + 1 };
+    if (sim_state.in_space(bottom_pos) && sim_state.particle_at(bottom_pos).type == ParticleType::e_null) {
+        sim_state.swap(particle_pos, bottom_pos);
+        return;
+    }
+
+    int rand_side = GetRandomValue(0, 1);
+    if (rand_side == 0) {
+        rand_side = -1;
+    }
+    Vector2i side_pos { particle_pos.x + rand_side, particle_pos.y + 1 };
+    if (sim_state.in_space(side_pos) && sim_state.particle_at(side_pos).type == ParticleType::e_null) {
+        sim_state.swap(particle_pos, side_pos);
+        return;
+    }
+}
+
+void update_water(SimState& sim_state, Vector2i particle_pos)
+{
+    Vector2i bottom_pos { particle_pos.x, particle_pos.y + 1 };
+    if (sim_state.in_space(bottom_pos) && sim_state.particle_at(bottom_pos).type == ParticleType::e_null) {
+        sim_state.swap(particle_pos, bottom_pos);
+        return;
+    }
+
+    int rand_side = GetRandomValue(0, 1);
+    if (rand_side == 0) {
+        rand_side = -1;
+    }
+    Vector2i side_pos { particle_pos.x + rand_side, particle_pos.y };
+    if (sim_state.in_space(side_pos) && sim_state.particle_at(side_pos).type == ParticleType::e_null) {
+        sim_state.swap(particle_pos, side_pos);
+        return;
+    }
+}
+
+void update_particle(SimState& sim_state, Vector2i particle_pos)
+{
+    switch (sim_state.particle_at(particle_pos).type) {
+    case ParticleType::e_salt:
+        update_salt(sim_state, particle_pos);
+        break;
+    case ParticleType::e_water:
+        update_water(sim_state, particle_pos);
+        break;
+    case ParticleType::e_null:
+        break;
+    }
+}
+
 void update_sim(SimState& sim_state, std::mt19937& rand_engine)
 {
     std::vector<int> rand_indices;
@@ -75,107 +127,97 @@ void update_sim(SimState& sim_state, std::mt19937& rand_engine)
     std::shuffle(rand_indices.begin(), rand_indices.end(), rand_engine);
 
     for (int i : rand_indices) {
-        if (sim_state.space.at(i).type == ParticleType::e_null) {
-            continue;
-        }
+        update_particle(sim_state, sim_state.get_pos(i));
+    }
+}
 
-        Vector2i pos = sim_state.get_pos(i);
+void draw_particle(rl::Image& render_image, const SimState& sim_state, Vector2i pos)
+{
+    switch (sim_state.particle_at(pos).type) {
+    case ParticleType::e_null:
+        render_image.DrawPixel(pos.x, pos.y, rl::Color::Black());
+        break;
+    case ParticleType::e_salt:
+        render_image.DrawPixel(pos.x, pos.y, rl::Color::FromHSV(0.0f, 0.0f, sim_state.particle_at(pos).shade));
+        break;
+    case ParticleType::e_water:
+        render_image.DrawPixel(pos.x, pos.y, rl::Color::FromHSV(243.0f, 0.9f, sim_state.particle_at(pos).shade));
+        break;
+    }
+}
 
-        if (sim_state.in_space({ pos.x, pos.y + 1 })) {
-            Vector2i bottom_pos { pos.x, pos.y + 1 };
-            if (sim_state.particle_at(bottom_pos).type == ParticleType::e_null) {
-                if (GetRandomValue(1, 2) == 1) {
-                    sim_state.swap(pos, bottom_pos);
-                }
-                continue;
-            }
-            else {
-                int val = GetRandomValue(0, 1);
-                if (val == 0) {
-                    val = -1;
-                }
-                Vector2i diag_pos { pos.x + val, pos.y + 1 };
-                if (sim_state.in_space(diag_pos) && sim_state.particle_at(diag_pos).type == ParticleType::e_null) {
-                    sim_state.swap(pos, diag_pos);
-                    continue;
-                }
-            }
-        }
-
-        if (sim_state.particle_at(pos).type == ParticleType::e_water) {
-            int val = GetRandomValue(0, 1);
-            if (val == 0) {
-                val = -1;
-            }
-            Vector2i side_pos { pos.x + val, pos.y };
-            if (sim_state.in_space(side_pos) && sim_state.particle_at(side_pos).type == ParticleType::e_null) {
-                sim_state.swap(pos, side_pos);
-                continue;
-            }
+void draw_column(rl::Image& render_image, const SimState& sim_state, int start_col, int col_width)
+{
+    for (int x = start_col * col_width; x < (start_col + 1) * col_width; x++) {
+        for (int y = 0; y < sim_state.height; y++) {
+            draw_particle(render_image, sim_state, { x, y });
         }
     }
-
-    //    for (int x = 0; x < sim_state.width; x++) {
-    //        for (int y = 0; y < sim_state.height; y++) {
-    //            size_t index = get_index(x, y, sim_state.width);
-    //            if (prev_space.at(index).exists) {
-    //                if (y + 1 < sim_state.height) {
-    //                    if (!prev_space.at(get_index(x, y + 1, sim_state.width)).exists) {
-    //                        std::swap(sim_state.space[index], sim_state.space[get_index(x, y + 1, sim_state.width)]);
-    //                    }
-    //                    else {
-    //                        int val = GetRandomValue(0, 1);
-    //                        if (val == 0) {
-    //                            val = -1;
-    //                        }
-    //                        rl::Vector2 pos((float)(x + val), (float)(y + 1));
-    //                        if (pos.CheckCollision(rl::Rectangle(0, 0, (float)sim_state.width,
-    //                        (float)sim_state.height))) {
-    //                            if (!prev_space.at(get_index((int)pos.x, (int)pos.y, sim_state.width)).exists) {
-    //                                std::swap(
-    //                                    sim_state.space[index],
-    //                                    sim_state.space[get_index((int)pos.x, (int)pos.y, sim_state.width)]);
-    //                            }
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    }
 }
 
 void draw_sim(rl::Image& render_image, const SimState& sim_state, BS::thread_pool& pool)
 {
-    for (int t = 0; t < 8; t++) {
-        pool.push_task([t, &sim_state, &render_image] {
-            for (int x = t * 40; x < (t + 1) * 40; x++) {
-                for (int y = 0; y < sim_state.height; y++) {
-                    int index = sim_state.get_index({ x, y });
-                    if (sim_state.space.at(index).type == ParticleType::e_salt) {
-                        render_image.DrawPixel(x, y, rl::Color::FromHSV(0.0f, 0.0f, sim_state.space.at(index).shade));
-                    }
-                    else if (sim_state.space.at(index).type == ParticleType::e_water) {
-                        render_image.DrawPixel(x, y, rl::Color::FromHSV(243.0f, 0.9f, 1.0f));
-                    }
-                    else {
-                        render_image.DrawPixel(x, y, rl::Color::Black());
-                    }
-                }
-            }
+    for (int col = 0; col < 8; col++) {
+        pool.push_task([col, &sim_state, &render_image] {
+            draw_column(render_image, sim_state, col, 40);
         });
     }
     pool.wait_for_tasks();
-    //    for (int x = 0; x < sim_state.width; x++) {
-    //        for (int y = 0; y < sim_state.height; y++) {
-    //            size_t index = get_index(x, y, sim_state.width);
-    //            if (sim_state.space.at(index).exists) {
-    //                render_image.DrawPixel(x, y, rl::Color::FromHSV(0.0f, 0.0f, sim_state.space.at(index).shade));
-    //            }
-    //            else {
-    //                render_image.DrawPixel(x, y, rl::Color::Black());
-    //            }
-    //        }
-    //    }
+}
+
+void main_loop(
+    const int screen_width,
+    const int screen_height,
+    SimState& sim_state,
+    util::FixedLoop& fixed_loop,
+    rl::Image& render_image,
+    rl::Texture& sim_texture,
+    std::mt19937& rand_engine,
+    BS::thread_pool& thread_pool)
+{
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+        rl::Vector2 mouse_pos = GetMousePosition();
+        Vector2i sim_pos { (int)mouse_pos.x, (int)mouse_pos.y };
+        if (sim_state.in_space(sim_pos)) {
+            sim_state.particle_at(sim_pos).type = ParticleType::e_salt;
+            sim_state.particle_at(sim_pos).shade = (float)GetRandomValue(750, 1000) / 1000.0f;
+        }
+    }
+    else if (IsMouseButtonDown(MOUSE_MIDDLE_BUTTON)) {
+        rl::Vector2 mouse_pos = GetMousePosition();
+        Vector2i sim_pos { (int)mouse_pos.x, (int)mouse_pos.y };
+        if (sim_state.in_space({ (int)mouse_pos.x, (int)mouse_pos.y })) {
+            sim_state.particle_at(sim_pos).type = ParticleType::e_water;
+            sim_state.particle_at(sim_pos).shade = 1.0f;
+        }
+    }
+    else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+        rl::Vector2 mouse_pos = GetMousePosition();
+        Vector2i sim_pos { (int)mouse_pos.x, (int)mouse_pos.y };
+        if (sim_state.in_space(sim_pos)) {
+            sim_state.particle_at(sim_pos).type = ParticleType::e_null;
+        }
+    }
+
+    fixed_loop.update(20, [&]() {
+        update_sim(sim_state, rand_engine);
+    });
+
+    draw_sim(render_image, sim_state, thread_pool);
+
+    sim_texture.Update(render_image.data);
+
+    BeginDrawing();
+    {
+        ClearBackground(rl::Color::Black());
+
+        sim_texture.Draw(
+            rl::Rectangle(0, 0, (float)sim_state.width, (float)sim_state.height),
+            rl::Rectangle(0, 0, (float)screen_width, (float)screen_height));
+
+        DrawFPS(10, 10);
+    }
+    EndDrawing();
 }
 
 void run()
@@ -190,16 +232,13 @@ void run()
     SimState sim_state { .width = 320, .height = 240 };
     sim_state.space = std::vector<Particle>();
     for (int i = 0; i < sim_state.width * sim_state.height; i++) {
-        sim_state.space.push_back(Particle {});
+        sim_state.space.push_back({});
     }
     rl::Image render_image = rl::Image(sim_state.width, sim_state.height);
 
-    for (int y = 0; y < sim_state.height; y++) {
-        for (int x = 0; x < sim_state.width; x++) {
-            render_image.DrawPixel(
-                rl::Vector2((float)x, (float)y),
-                rl::Color::FromHSV(0.0f, 0.0f, ((float)GetRandomValue(0, 1000)) / 1000.0f));
-        }
+    for (int i = 0; i < sim_state.space.size(); i++) {
+        Vector2i pos = sim_state.get_pos(i);
+        render_image.DrawPixel(pos.x, pos.y, rl::Color::Black());
     }
 
     SetMouseScale(320.0f / 1200.0f, 320.0f / 1200.0f);
@@ -213,52 +252,7 @@ void run()
     std::mt19937 rand_engine;
 
     while (!window.ShouldClose()) {
-
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-            rl::Vector2 mouse_pos = GetMousePosition();
-            if (sim_state.in_space({ (int)mouse_pos.x, (int)mouse_pos.y })) {
-                sim_state.space.at(sim_state.get_index({ (int)mouse_pos.x, (int)mouse_pos.y })).type
-                    = ParticleType::e_salt;
-                sim_state.space.at(sim_state.get_index({ (int)mouse_pos.x, (int)mouse_pos.y })).shade
-                    = (float)GetRandomValue(750, 1000) / 1000.0f;
-            }
-        }
-        else if (IsMouseButtonDown(MOUSE_MIDDLE_BUTTON)) {
-            rl::Vector2 mouse_pos = GetMousePosition();
-            if (sim_state.in_space({ (int)mouse_pos.x, (int)mouse_pos.y })) {
-                sim_state.space.at(sim_state.get_index({ (int)mouse_pos.x, (int)mouse_pos.y })).type
-                    = ParticleType::e_water;
-                sim_state.space.at(sim_state.get_index({ (int)mouse_pos.x, (int)mouse_pos.y })).shade
-                    = (float)GetRandomValue(750, 1000) / 1000.0f;
-            }
-        }
-        else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-            rl::Vector2 mouse_pos = GetMousePosition();
-            if (sim_state.in_space({ (int)mouse_pos.x, (int)mouse_pos.y })) {
-                sim_state.space.at(sim_state.get_index({ (int)mouse_pos.x, (int)mouse_pos.y })).type
-                    = ParticleType::e_null;
-            }
-        }
-
-        fixed_loop.update(20, [&]() {
-            update_sim(sim_state, rand_engine);
-        });
-
-        draw_sim(render_image, sim_state, pool);
-
-        texture.Update(render_image.data);
-
-        BeginDrawing();
-        {
-            ClearBackground(rl::Color::Black());
-
-            texture.Draw(
-                rl::Rectangle(0, 0, (float)sim_state.width, (float)sim_state.height),
-                rl::Rectangle(0, 0, screen_width, screen_height));
-
-            DrawFPS(10, 10);
-        }
-        EndDrawing();
+        main_loop(screen_width, screen_height, sim_state, fixed_loop, render_image, texture, rand_engine, pool);
     }
 }
 
